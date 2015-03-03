6c39a776bbbac0d8d11d206fad56c091032d6b50
Xavier Roger-Machart <xrm@omegacube.fr>
2015-03-03 19:22:42 +0100

    First version of the host details page

--- hokuto/module/web/configservice.py
+++ hokuto/module/web/configservice.py
@@ -26,7 +26,11 @@ from pynag.Parsers import config
 
 from flask import jsonify, render_template, abort
 from flask.ext.login import login_required
+from wtforms import Form, TextField, SelectField, SelectMultipleField, TextAreaField,  validators
+from wtforms.fields.html5 import IntegerField, URLField
 from werkzeug.contrib.cache import SimpleCache
+from shinken.property import none_object
+import shinken.objects
 
 from . import app, cache
 
@@ -56,6 +60,22 @@ def _readStr(source, attr, default = None):
         return unicode(source[attr], errors='replace')
     else:
         return default
+
+def _parsetype(type):
+    """ 
+    Checks if the specified type name is a template, and returns the actual
+    nagios/shinken data type and a boolean telling if it was a template.
+    
+    For example if you pass 'hosttemplate' you'll get ('host', True)
+    But if you pass 'host' you'll get ('host', False)
+    """
+    istemplate = False
+    if type.endswith('s'):
+        type = type[:-1] # Remove the trailing 's'
+    if type.endswith('template'):
+        istemplate = True
+        type = type[:-8] # Remove the trailing 'template'
+    return (type, istemplate)
         
 def _normalizestrings(data):
     """ 
@@ -86,13 +106,8 @@ def _normalizestrings(data):
 @login_required
 def conf_getdatalist(type):
     """ Returns a JSON object containing all the data for objects of the specified type """
-    istemplate = False
-    if type.endswith('templates'):
-        istemplate = True
-        type = type[:-9]
-    else:
-        type = type[:-1] # Just remove the trailing s
-        
+    (type, istemplate) = _parsetype(type)
+    
     if type not in _typekeys:
         return jsonify({'success': False, 'errormessage': 'Unknown object type'}), 404
     key = _typekeys[type]
@@ -113,9 +128,176 @@ def conf_getdatalist(type):
 def config_list():
     return render_template('config/list.html')
 
+@app.route('/config/<type>/<id>')
+@login_required
+def config_details(type, id):
+    print 'lel'
+    # try to get the target element
+    (type, istemplate) = _parsetype(type)
+    print type, istemplate
+    if type not in _typekeys:
+        abort(404) # Unsupported type
+    
+    conf = _getconf()
+    typekey = 'all_' + type
+    if typekey not in conf.data:
+        abort(404) # No element of this type
+    primkey = _typekeys[type]
+    target = next((e for e in conf.data[typekey] if primkey in e and e[primkey] == id), None)
+    if target is None:
+        abort(404) # Unknown id
+    
+    form = HostForm()
+    _fillform(form, target)
+    _annotateform(form, target)
+    
+    return render_template('config/details.html', type=type, id=id, data=_normalizestrings(target), form=form)
+    
 @app.route('/config/canwrite')
 @login_required
 def config_can_edit():
     confpath = paths.find_main_configuration_file()
     result = os.access(confpath, os.W_OK)
     return jsonify({'success': True, 'data':result})
+
+# #########################################################################################################
+# Form tools
+
+def _fillform(form, data):
+    for k,v in data['meta']['defined_attributes'].iteritems():
+        field = getattr(form, k, None)
+        if field is not None:
+            print 'Setting field {0} to {1}'.format(k, v)
+            if isinstance(field, SelectMultipleField):
+                field.process(None, v.split(','))
+            else:
+                field.process(None, v)
+                
+def _annotateform(form, data):
+    typedata = getattr(shinken.objects, data['meta']['object_type'].title(), None)
+    if typedata is None:
+        return
+    for field in form:
+        propdata = None
+        if field.name in typedata.properties:
+            propdata = typedata.properties[field.name]
+        # Is the value inherited ?
+        if field.name in data['meta']['inherited_attributes']:
+            desc = _createannotation(data['meta']['inherited_attributes'][field.name], True)
+            if desc is not None:
+                field.description = desc
+        elif propdata is not None:
+            if propdata.default != none_object:
+                desc = _createannotation(propdata.default, False)
+                if desc is not None:
+                    field.description = desc
+
+def _createannotation(value, inherited):
+    """ 
+    Generates a string that describes the default value of a property 
+    
+    value contains the default value applied
+    inherited tells if the default value is applied because it's inherited
+    (True) or just because no value is available (False)
+    """
+    empty = False
+    if value is None:
+        empty = True
+    elif hasattr(value, '__len__') and len(value) == 0:
+        empty = True
+    
+    if empty:
+        if inherited:
+            return 'Default value: empty (inherited)'
+        else:
+            return None
+    
+    if isinstance(value, list):
+        value = ', '.join(value)
+        
+    if isinstance(value, bool):
+        if value:
+            value = 'Yes'
+        else:
+            value = 'No'
+        
+    value = 'Default: {0}'.format(value)
+    if inherited:
+        value = value + ' (inherited)'
+    return value
+            
+def _listobjects(type, key = None):
+    if key is None:
+        key = type + '_name'
+    conf = _getconf()
+    typekey = 'all_' + type
+    if typekey in conf.data:
+        result = [i[key] for i in conf.data[typekey] if key in i]
+        result.sort()
+        return result
+    else:
+        return []
+
+def _listobjects_choices(type, addempty = False, key = None):
+    """ Gets a list from _listobjects and formats it so it can work with a SelectField """
+    data = _listobjects(type, key)
+    data = [(i,i) for i in data]
+    if addempty:
+        data.insert(0, ('', '<unspecified>'))
+    return data
+    
+def _listboolean_choices():
+    return [('', '<unspecified>'), ('y', 'Yes'), ('n', 'No')]
+    
+# #########################################################################################################
+# Forms
+
+class HostForm(Form):
+    #Description
+    host_name = TextField('Host name')
+    alias = TextField('Alias')
+    display_name = TextField('Display name')
+    address = TextField('Address')
+    notes = TextAreaField('Notes')
+    notes_url = URLField('Notes URL')
+    action_url = URLField('Action URL')
+    
+    #Structure
+    parents = SelectMultipleField('Parents', choices=_listobjects_choices('host'))
+    hostgroups = SelectMultipleField('Host groups', choices=_listobjects_choices('hostgroup'))
+    
+    #Checking
+    check_command = SelectField('Check command', choices=_listobjects_choices('command', True))
+    initial_state = SelectField('Initial state', choices=[('', '<unspecified>'), ('o','Up (o)'), ('d','Down (d)'), ('u','Unknown (u)')])
+    max_check_attempts = IntegerField('Maximum check attempts', validators=[validators.Optional(), validators.NumberRange(0)])
+    check_interval = IntegerField('Check interval')
+    retry_interval = IntegerField('Retry interval')
+    active_checks_enabled = SelectField('Enable active notifications', choices=_listboolean_choices())
+    passive_checks_enabled = SelectField('Enable passive notifications', choices=_listboolean_choices())
+    check_period = SelectField('Check command', choices=_listobjects_choices('timeperiod', True))
+    obsess_over_host = SelectField('Obsess over host', choices=_listboolean_choices())
+    check_freshness = SelectField('Check freshness', choices=_listboolean_choices())
+    freshness_threshold = IntegerField('Freshness threshold')
+    
+    #Status management
+    event_handler = SelectField('Event handler', choices=_listobjects_choices('command', True))
+    event_handler_enabled = SelectField('Event handler enabled', choices=_listboolean_choices())
+    low_flap_threshold = IntegerField('Low flap threshold')
+    high_flap_threshold = IntegerField('High flap threshold')
+    flap_detection_enabled = SelectField('Flap detection enabled', choices=_listboolean_choices())
+    flap_detection_options = SelectMultipleField('Flap detection options', choices=[('o','Up (o)'), ('d','Down (d)'), ('u','Unknown (u)')])
+    retain_status_information = SelectField('Retain status info', choices=_listboolean_choices())
+    retain_nonstatus_information = SelectField('Retain non-status info', choices=_listboolean_choices())
+    
+    #Notifications
+    notifications_enabled = SelectField('Enable notifications', choices=_listboolean_choices())
+    contacts = SelectMultipleField('Contacts', choices=_listobjects_choices('contact'))
+    contact_groups = SelectMultipleField('Contact groups', choices=_listobjects_choices('contactgroup'))
+    notification_interval = IntegerField('Notification interval')
+    first_notification_delay = IntegerField('First notification delay')
+    notification_period = SelectField('Notification period', choices=_listobjects_choices('timeperiod', True))
+    notification_options = SelectMultipleField('Notification options', choices=[('d','Down (d)'), ('u','Unknown (u)'), ('r', 'Recovery (r)'), ('f', 'Flapping (f)'), ('s', 'Scheduled downtime starts/ends (s)'), ('n', 'None (n)')])
+    
+    # Misc.
+    process_perf_data = SelectField('Process perf data', choices=_listboolean_choices())
+    stalking_options = SelectMultipleField('Stalking options', choices=[('o','Up (o)'), ('d','Down (d)'), ('u','Unknown (u)')])
---
+++ hokuto/module/web/static/scripts/config.details.js
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Omega Noc
+ * Copyright Omega Noc (C) 2015 Omega Cube and contributors
+ * Xavier Roger-Machart, xrm@omegacube.fr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+define(['jquery', 'config.data', 'select2', 'jquery.validate'], function(jQuery, structure) {
+    jQuery(function() {
+        var data = _conf_details_data;
+        var type = _conf_details_type;
+        var type_data = structure[type];
+        
+        // Set up validation
+        for(i in type_data.properties) {
+            // Does a field exist for this property ?
+            var field = jQuery('#' + i);
+            if(field.length > 0) {
+                // Yes !
+                var fieldtype = type_data.properties[i].type;
+                //var multiple = false;
+                if(fieldtype.indexOf('*') == 0) {
+                    //multiple = true;
+                    fieldtype = fieldtype.substr(1); // Remove the leading *
+                }
+                
+                // Basic types
+                switch(fieldtype) {
+                    case 'integer':
+                        // For now all integers should be zero or positive
+                        field.attr('min', '0');
+                        break;
+                    case 'shortname':
+                        field.attr('required', 'required');
+                        field.attr('pattern', '^[^`~!$%^&*"|\'<>?,()=]+$');
+                        break;
+                }
+            }
+        }
+        
+        // Activate validation
+        jQuery('form.details-form').validate();
+        
+        // Select2 lists
+        jQuery('select[multiple]').select2({
+            width: '400px',
+        });
+        
+        // Open / collapse fieldsets
+        jQuery('form > fieldset > legend').click(function() {
+            jQuery(this).parent().toggleClass('collapsed');
+        });
+    });
+});
